09b100cf3dd456a7f5c267cf388ea07f465d78cd
Yura <bobr515yura@yandex.ru>
2024-01-17 19:24:15 +0300

    MyTask

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -25,6 +25,13 @@ class Game {
       При неправильном вводе символа - this.fail();
       DOM-элемент текущего символа находится в свойстве this.currentSymbol.
      */
+      document.addEventListener('keydown', (e) => {
+        if((e.key).toLowerCase() === this.currentSymbol.textContent.toLowerCase()){
+          this.success();
+        } else {
+          this.fail();
+        } 
+      });
   }
 
   success() {
